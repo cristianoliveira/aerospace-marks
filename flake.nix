dbefbd8c9471a7b2fab2725730e99b0027a37d2f
cristianoliveira <contato@cristianoliveira.com.br>
2025-05-11 13:20:43 +0200

    chore(dx): adds golang deps

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "aerospace-marks: I3wm like marks feature";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, utils, ... }: 
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+          ];
+        };
+
+        packages = {
+          default = pkgs.hello;
+        };
+    });
+}
