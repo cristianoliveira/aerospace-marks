1bc3b82fd50c18820445d751a123a28daeace654
cristianoliveira <contato@cristianoliveira.com.br>
2025-05-15 07:39:44 +0200

    chore(ci): bump nix version

--- nix/package-default.nix
+++ nix/package-default.nix
@@ -11,10 +11,10 @@
       owner = "cristianoliveira";
       repo = "aerospace-marks";
       rev = version;
-      sha256 = "sha256-TQAcKtIid1+zP6dRixcuH6jZZHko1+ujZH2Kya+Snq8=";
+      sha256 = "sha256-WgAH+ILK7IDrYiBBZDwSEfSTebyjF65WlfCWNsqnV8M=";
     };
 
-    vendorHash = "sha256-1pCoyPjokM+Qm+mbNQLCOmVVqwHDUxJ10RyvLV/becQ=";
+    vendorHash = "sha256-0s4aCxaWRolYdLadouszxnrlT+9x+PpJiOaQ4pUPhAM=";
 
     ldflags = [
       "-s" "-w"
