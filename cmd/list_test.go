8eb29a0aa294f8655e5752c76e61a8eb77e030e2
cristianoliveira <contato@cristianoliveira.com.br>
2025-05-17 15:32:46 +0200

    tests: adds test for missing marks

--- cmd/list_test.go
+++ cmd/list_test.go
@@ -62,4 +62,67 @@ func TestListCommand(t *testing.T) {
 			"mark2 | 2 | app2 | title2",
 		})
 	})
+
+	t.Run("shows no marked window found", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		storageDbClient, _ := mocks.MockStorageDbClient(ctrl)
+		storageDbClient.EXPECT().
+			QueryAll(gomock.Any()).
+			Return(
+				[]storage.Mark{
+					{
+						WindowID: "1",
+						Mark:     "mark1",
+					},
+					{
+						WindowID: "2",
+						Mark:     "mark2",
+					},
+				}, nil,
+			)
+
+		mockAeroSpaceConnection, _ := mocks.MockAerospaceConnection(ctrl)
+		aerospaceResponseOutput := []string{
+			"111 | app1 | title1",
+			"222 | app2 | title2",
+			"333 | app3 | title3",
+		}
+		mockAeroSpaceConnection.EXPECT().
+			SendCommand("list-windows", []string{"--all"}).
+			Return(
+				&aerospacecli.Response{
+					ServerVersion: "1.0",
+					StdOut:        strings.Join(aerospaceResponseOutput, "\n"),
+					StdErr:        "",
+					ExitCode:      0,
+				}, nil).Times(1)
+
+		out, err := testutils.CmdExecute(rootCmd, "list")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		result := strings.TrimSpace(out)
+		assert.Equal(t, result, "No marked window found")
+	})
+
+	t.Run("shows no marks found", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		defer ctrl.Finish()
+
+		storageDbClient, _ := mocks.MockStorageDbClient(ctrl)
+		storageDbClient.EXPECT().
+			QueryAll(gomock.Any()).
+			Return([]storage.Mark{}, nil)
+
+		out, err := testutils.CmdExecute(rootCmd, "list")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		result := strings.TrimSpace(out)
+		assert.Equal(t, result, "No marks found")
+	})
 }
