47cbd76a3e5277311b06295b2a0aff28848889f2
cristianoliveira <contato@cristianoliveira.com.br>
2025-05-18 08:03:48 +0200

    feat: allows check version

--- cmd/main.go
+++ cmd/main.go
@@ -26,18 +26,12 @@ mark --add|--replace [--toggle] <identifier>
 
 See: man 5 sway
 `,
+	Version: VERSION,
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// NOTE: add here global flags
+	// rootCmd.Flags().BoolP("version", "v", false, "Print version information")
 }
 
 func Run() {
@@ -46,3 +40,8 @@ func Run() {
 		os.Exit(1)
 	}
 }
+
+// THIS IS GENERATED DON'T EDIT
+// NOTE: to update delete the var and
+// run scripts/validate-version.sh
+var VERSION = "v0.0.1-20250518-75aee46"
