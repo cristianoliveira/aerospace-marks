f5bafd5c147a283ba3d3b458bc6ef1f753165d40
cristianoliveira <contato@cristianoliveira.com.br>
2025-05-20 15:31:58 +0200

    feat: adds command summon <mark>

---
+++ cmd/summon.go
@@ -0,0 +1,66 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"strconv"
+
+	"github.com/cristianoliveira/aerospace-marks/internal/aerospace"
+	"github.com/cristianoliveira/aerospace-marks/internal/stdout"
+	"github.com/cristianoliveira/aerospace-marks/internal/storage"
+	"github.com/spf13/cobra"
+)
+
+// SummonCmd represents the summon command
+func SummonCmd(
+	storageClient storage.MarkStorage,
+	aerospaceClient aerospace.AerosSpaceMarkWindows,
+) *cobra.Command {
+	summonCmd := &cobra.Command{
+		Use:   "summon",
+		Short: "Summon a marked window to current workspace",
+		Long: `Summon a marked window to current workspace.
+
+Similar to 'aerospace summon-workspace' but for marked windows to current workspace.
+`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return fmt.Errorf("No identifier provided to focus")
+			}
+
+			mark := args[0]
+
+			// Get window ID by mark
+			windowID, err := storageClient.GetWindowIDByMark(mark)
+			if err != nil {
+				return stdout.ErrorAndExit(err)
+			}
+			if windowID == "" {
+				return stdout.ErrorAndExitf("no window found for mark '%s'", mark)
+			}
+
+			workspace, err := aerospaceClient.Client().GetFocusedWorkspace()
+			if err != nil {
+				return stdout.ErrorAndExit(err)
+			}
+
+			// FIXME: windowsID as number
+			intWindowID, err := strconv.Atoi(windowID)
+			if err != nil {
+				return stdout.ErrorAndExitf("invalid window ID '%s'", windowID)
+			}
+
+			// focus to window by ID
+			err = aerospaceClient.Client().MoveWindowToWorkspace(intWindowID, workspace.Workspace)
+			if err != nil {
+				return stdout.ErrorAndExit(err)
+			}
+
+			return nil
+		},
+	}
+
+	return summonCmd
+}
