01713553a951085210d76d2603052307f9ad0a9a
cristianoliveira <contato@cristianoliveira.com.br>
2025-06-01 20:25:45 +0200

    feat(logs): ensure errors are logged

--- internal/stdout/error.go
+++ internal/stdout/error.go
@@ -3,6 +3,8 @@ package stdout
 import (
 	"fmt"
 	"os"
+
+	"github.com/cristianoliveira/aerospace-marks/internal/logger"
 )
 
 var ShouldExit = true
@@ -10,6 +12,8 @@ var ShouldExit = true
 // ErrorAndExit is a function that prints an error message to stderr and exits the program with a non-zero status code.
 func ErrorAndExit(err error) {
 	if err != nil {
+		logger := logger.GetDefaultLogger()
+		logger.LogError("ERROR:", "msg", err)
 		errorMessage := fmt.Errorf("error: %v", err)
 		fmt.Fprintln(os.Stderr, err.Error())
 		if ShouldExit {
@@ -21,6 +25,8 @@ func ErrorAndExit(err error) {
 }
 
 func ErrorAndExitf(format string, a ...any) {
+	logger := logger.GetDefaultLogger()
+	logger.LogError("ERROR", "msg", fmt.Errorf(format, a...))
 	fmt.Fprintln(os.Stderr, fmt.Errorf(format, a...))
 	if ShouldExit {
 		os.Exit(1)
