f0f3e331c6bc89e53ddc372e137d4eec162806eb
cristianoliveira <contato@cristianoliveira.com.br>
2025-06-01 20:25:18 +0200

    fix(focus): wait 100ms to set focus and avoid issues

--- cmd/focus.go
+++ cmd/focus.go
@@ -6,9 +6,11 @@ package cmd
 import (
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/cristianoliveira/aerospace-marks/internal/aerospace"
 	"github.com/cristianoliveira/aerospace-marks/internal/cli"
+	"github.com/cristianoliveira/aerospace-marks/internal/logger"
 	"github.com/cristianoliveira/aerospace-marks/internal/stdout"
 	"github.com/cristianoliveira/aerospace-marks/internal/storage"
 	"github.com/spf13/cobra"
@@ -31,29 +33,39 @@ Moves focus to the first window marked with the specified identifier.
 			cli.ValidateArgIsNotEmpty,
 		),
 		Run: func(cmd *cobra.Command, args []string) {
+			logger := logger.GetDefaultLogger()
 			mark := args[0]
+			logger.LogDebug("FocusCmd called", "mark", mark)
 
 			windowID, err := storageClient.GetWindowIDByMark(mark)
 			if err != nil {
 				stdout.ErrorAndExit(err)
 				return
 			}
+
 			if windowID == "" {
 				stdout.ErrorAndExitf("empty window id for mark '%s'", mark)
 				return
 			}
+			logger.LogDebug("Window found", "windowID", windowID)
 
 			intWindowID, err := strconv.Atoi(windowID)
 			if err != nil {
 				stdout.ErrorAndExitf("invalid window ID '%s'", windowID)
 				return
 			}
+
+			// The program is too fast, what a problem to have!
+			// Sleep for half a second to ensure the window is ready
+			time.Sleep(100 * time.Millisecond)
+
 			err = aerospaceClient.Client().SetFocusByWindowID(intWindowID)
 			if err != nil {
 				stdout.ErrorAndExit(err)
 				return
 			}
 
+			logger.LogDebug("Focus set", "windowID", windowID)
 			fmt.Printf("Focus moved to window ID %s\n", windowID)
 		},
 	}
